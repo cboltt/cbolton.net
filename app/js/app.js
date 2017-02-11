a274681da9716a1c5537399c1b618ad23fe19c9e
cbolton97 <chris.bolton97@gmail.com>
2017-02-10 22:08:54 -0800

    adds scss support

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import '../scss/app.scss';
 
 import App from 'views/App';
 import Landing from 'views/Landing';

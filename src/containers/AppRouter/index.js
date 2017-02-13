ebb373f54df140869982cb1146c73cd703b8a07b
cbolton97 <chris.bolton97@gmail.com>
2017-02-13 14:42:47 -0800

    fix router issue

--- src/containers/AppRouter/index.js
+++ src/containers/AppRouter/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from '../App';
 import Landing from '../Landing';

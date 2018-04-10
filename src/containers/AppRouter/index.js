df553e9a9d623293d494ac5f8b47e3d54f3da643
cbolton97 <chris.bolton97@gmail.com>
2018-04-09 18:52:14 -0700

    Hopefully fix redirect issue

--- src/containers/AppRouter/index.js
+++ src/containers/AppRouter/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from '../App';
 import Landing from '../Landing';
@@ -8,7 +8,7 @@ import Landing from '../Landing';
 export default class AppRouter extends Component {
   render() {
     return (
-      <Router history={ hashHistory }>
+      <Router history={ browserHistory }>
         <Route path='/' component={ App }>
           <IndexRoute component={ Landing } />
         </Route>

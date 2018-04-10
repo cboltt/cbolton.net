14793a5f1e1ac3f4737619e18ea78959df10ccfc
cbolton97 <chris.bolton97@gmail.com>
2018-04-09 22:57:21 -0700

    Updates spinner, fixes spelling

--- src/components/Landing/TabCount/index.jsx
+++ src/components/Landing/TabCount/index.jsx
@@ -14,7 +14,7 @@ const TabCount = ({ tabCount }) => {
   let details = "Looks like my browser may have just restarted"
 
   if(tabCountInt >= 30) {
-    details = "Looks like things are a bit busy."
+    details = "It's a lot, I know."
   } else if(tabCount >= 1) {
     details = "It's not too bad at the moment."
   } else {
@@ -32,11 +32,8 @@ const TabCount = ({ tabCount }) => {
     <div { ...classes() }>
       <h1 { ...classes('count') }>
         <div { ...classes('spinner') }>{ tabCount }</div>
-        <div { ...classes('bar') }></div>
       </h1>
-      <p { ...classes('details') }>
-        This is the count of how many Chrome tabs I have open on my computer. { details }
-      </p>
+      <p  { ...classes('details')}>open browser tabs. <br /> { details }</p>
     </div>
   );
 }

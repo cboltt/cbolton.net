3db3b844a962a55f491cd219a21539b927358f37
Chris Bolton <chris.bolton97@gmail.com>
2019-02-24 21:02:05 -0800

    initial header design

--- src/foundation/App/App.tsx
+++ src/foundation/App/App.tsx
@@ -4,12 +4,93 @@ import styles from './App.module.scss';
 export default function App() {
   return (
     <div className={styles.App}>
-      <h1 className={styles.Header}>
-        <span className={styles.First}>Chris</span>
-        <span className={styles.Second}>
-          Bo<span className={styles.Spinny}>l</span>ton
-        </span>
-      </h1>
+      <div className={styles.Header}>
+        <div className={styles.Name}>
+          <div className={styles.First}>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="C">
+                C
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="h">
+                h
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="r">
+                r
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="i">
+                i
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="s">
+                s
+              </span>
+            </div>
+          </div>
+          <div className={styles.Second}>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="B">
+                B
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="o">
+                o
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="l">
+                l
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="t">
+                t
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="o">
+                o
+              </span>
+            </div>
+            <div className={styles.LetterBox}>
+              <span className={styles.Letter} data-shadow="n">
+                n
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.Links}>
+          <ul>
+            <li>
+              <a href="mailto:hello@cbolton.net" target="_blank">
+                Email
+              </a>
+            </li>
+            <li>
+              <a href="http://medium.com/@cbolton97" target="_blank">
+                Medium
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/cbolton97/" target="_blank">
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href="http://github.com/cbolton97" target="_blank">
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

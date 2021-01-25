d902dbf037688a8b21cd92e126c0290e15a94e06
Chris Bolton <chris.bolton97@gmail.com>
2021-01-24 18:00:00 -0800

    final touches

--- src/App/Projects/Project/Project.tsx
+++ src/App/Projects/Project/Project.tsx
@@ -20,11 +20,11 @@ export default function Project({icon, color, title, description, url}: Props) {
       <span className={styles.Icon} role="presentation" style={{color: color ? color : 'inherit'}}>
         {icon}
       </span>
-      <div>
+      <div className={styles.Content}>
         <h3 className={styles.Title}>{title}</h3>
         <p className={styles.Description}>{description}</p>
-        <Link label={hostname} accessibilityLabel={`Visit ${title}`} url={url} />
       </div>
+      <Link label={hostname} accessibilityLabel={`Visit ${title}`} url={url} />
     </div>
   );
 }

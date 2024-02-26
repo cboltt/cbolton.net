df9d72ab40e1609c6ba057043e4730ba3cecca73
Chris Bolton <chris.bolton97@gmail.com>
2024-02-25 21:46:31 -0800

    adds prerendering

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { htmlPrerender } from "vite-plugin-html-prerender";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    htmlPrerender({
+      /**
+       * Required: Output directory of `vite build`.
+       */
+      staticDir: path.join(__dirname, "dist"),
+
+      /**
+       * Required: List of routes to pre-render.
+       */
+      routes: ["/"],
+    }),
+  ],
+});

989d97c5a66880ed931fbfca353214ef91a5d719
Chris Bolton <chris.bolton97@gmail.com>
2024-11-29 16:01:38 -0500

    copy

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -20,7 +20,7 @@ export default function App() {
             <p> Right now, I'm building the web experience of Pinecone, a
             high-performance vector database for AI applications.</p>
             <p>
-              I've been a member of world-class engineering teams since I was in
+              I've been a member of world-class product teams since I was in
               high school. My most recent engineering roles have been at Microsoft (Teams),
               Twitter (Trust and Safety), and Shopify (Product Operations).
             </p>

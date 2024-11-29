adc7cef24f47807e5fa9d8d2a198a5c0991c0bbe
Chris Bolton <chris.bolton97@gmail.com>
2024-11-29 15:25:11 -0500

    copy edits

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -18,15 +18,16 @@ export default function App() {
                 Hey there! My name is Chris and I'm a senior software engineer
                 based in New York City.{" "}
               </strong>
-              Currently, I'm building the web experience of Pinecone, a
-              high-performance vector database for AI applications.
+             
             </p>
+            <p> Right now, I'm building the web experience of Pinecone, a
+            high-performance vector database for AI applications.</p>
             <p>
               I've been a member of world-class engineering teams since I was in
               high school. My most recent experiences include Microsoft (Teams),
               Twitter (Trust and Safety), and Shopify (Product Ops).
             </p>
-            <p>If you're interested in working with me, please get in touch.</p>
+            <p>If you're interested in working with me, please reach out.</p>
             <nav className={styles.Nav} aria-label="Related sites">
               {headerData.links.map((link) => (
                 <Link rel="me" key={link.url} {...link} />

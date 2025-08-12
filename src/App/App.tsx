6e782bf52e1f6ae86430aaeec07ddca29870f0f1
Chris Bolton <chris.bolton97@gmail.com>
2025-08-11 19:03:15 -0600

    updates location

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -13,16 +13,28 @@ export default function App() {
             <p>
               <strong>
                 Hey there! My name is Chris and I'm a staff software engineer
-                based in New York City.{" "}
+                based in Los Angeles.{" "}
               </strong>
-             
             </p>
-            <p>I'm building the web experience of <Link url="https://www.pinecone.io" label="Pinecone"/>, a
-            high-performance vector database for AI applications. I am also a Design and Technology Fellow at <Link url="https://www.faspe-ethics.org/about-us/" label="FASPE"/>.</p>
             <p>
-             With over a decade of product development experience, I take pride in building fast, accessible, and intuitive user interfaces.</p>
-            <p>My most recent engineering roles have been at Microsoft (Teams),
-              Twitter (Trust and Safety), and Shopify (Product Ops / Knowledge Management).
+              I'm building the web experience of{" "}
+              <Link url="https://www.pinecone.io" label="Pinecone" />, a
+              high-performance vector database for AI applications. I am also a
+              Design and Technology Fellow at{" "}
+              <Link
+                url="https://www.faspe-ethics.org/about-us/"
+                label="FASPE"
+              />
+              .
+            </p>
+            <p>
+              With over a decade of product development experience, I take pride
+              in building fast, accessible, and intuitive user interfaces.
+            </p>
+            <p>
+              My most recent engineering roles have been at Microsoft (Teams),
+              Twitter (Trust and Safety), and Shopify (Product Ops / Knowledge
+              Management).
             </p>
             <p>If you're interested in working with me, please reach out.</p>
             <nav className={styles.Nav} aria-label="Related sites">
